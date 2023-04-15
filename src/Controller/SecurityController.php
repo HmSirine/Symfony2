e184d0b95a47fc9b9565e57fc33213a576cf1b26
sirine hammami <hammamisyrinr28@gmail.com>
2023-04-16 00:22:02 +0100

    first commit

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class SecurityController extends AbstractController
+{
+    #[Route('/security', name: 'app_security')]
+    public function index(): Response
+    {
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+
+      
+}
